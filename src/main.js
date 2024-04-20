0f313de8a2c3d23181ed462263a22323969114a2
rarguet1 <rarguet1@umbc.edu>
2024-04-20 11:53:13 -0400

    added vuetify

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(Vuetify)
+app.mount('#app')
