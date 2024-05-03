0e255193a3eda91c6ef892378286282a05013afa
Jalen Brown <jbrown36@umbc.edu>
2024-05-03 13:30:25 -0400

    Update Tab3Page and add new images

--- src/views/Tab3Page.vue
+++ src/views/Tab3Page.vue
@@ -11,7 +11,6 @@ import "swiper/css/pagination";
 import "swiper/css/autoplay";
 
 const slides = [
-"all_year_streamgraph.png",
 "1990_streamgraph.png",
 "1991_streamgraph.png",
 "1992_streamgraph.png",
@@ -46,18 +45,20 @@ const currentStep = ref(null);
 const steps = [
   { 
     title: 'Marks & Channels',
-    content: `[insert image]`  
+    content: `<img src="/images/vises.webp" alt="marks + channels" style="border-radius: 5%;" />`  
   },
   { 
     title: 'What Are Marks & Channels?',
-    content: `<b>Marks</b> are basic geometric elements and <b>Channels</b> control their appearance. We consider these two aspects to be the <b>core building blocks</b> of modern visual encodings. `
+    content: `<b>Marks</b> are basic geometric elements and <b>Channels</b> control their appearance. We consider these two aspects to be the <b>core building blocks</b> of modern visual encodings. 
+    <img src="/images/marks.png" alt="marks" />
+    <img src="/images/channels.png" alt="channels" />`
   },
   { 
     title: 'Why Are They Important?',
     content: `Its significance as a key concept in constructing visualizations. As it serves as the core design space of visual encodings with two principles: <b>Expressiveness</b> and <b>Effectiveness</b>.
     <br><br>
     I will explore these concepts through my examples illustrating the uses of Marks & Channels within visualizations. 
-    <br><br>
+    <br><br><br><br>
     Starting with....
     `
   },
@@ -67,7 +68,7 @@ const steps = [
     <br><br>
     <ul>
       <li>
-        <img src="/images/LastGraph_example.svg.png" alt="example_1" width="600px" />
+        <img src="/images/LastGraph_example.svg.png" alt="streamgraph example" width="600px" />
       </li>
       <li>
         <p>Example of a symmetrical ThemeRiver of music listened to by a person</p>
@@ -76,51 +77,46 @@ const steps = [
   },
   { 
     title: 'Implementations',
-    content: `Here is my streamgraph using Matplotlib, Pandas, and Numpy:
-    <br><br>
-    **Figure 1** presents a revised streamgraph depicting the yearly market trends (MMR) by car make and year, featuring additional interactivity such as a scrollable legend, year slider, and reset buttons.
-    <br><br>
-    **Magnitude Channel**: Position, Area
-    <br><br>
-    **Identity Channel**: Spatial position, Color Hue`
+    content: `Here is my streamgraph using Matplotlib, Pandas, and Numpy:`
   },
   { 
     title: 'Results',
-    content: `In addressing the user task of 'Determining automotive market trends on a yearly basis,' I concluded that a streamgraph is the most suitable visualization. Users can explore yearly trends, car makes, and the Market Movement Ratio (MMR) through an interactive interface. Moreover, specific features, including the slider, scrollable legend, and reset button, offer detailed information to help users identify points of interest.
+    content: `In addressing the user task of <b>'Determining automotive market trends on a yearly basis,'</b> I found that a streamgraph is the most suitable visualization. Users can explore yearly trends, car makes, and the Market Movement Ratio (MMR) through an interactive interface. Moreover, specific features, including the slider, scrollable legend, and reset button, offer detailed information to help users identify points of interest.
     <br><br>
     Now, my next vis is....`
   },
   { 
     title: 'Choropleth Map',
-    content: `A **Choropleth map** is a type of statistical thematic map that utilizes color to represent data. The colors correspond to aggregates of data that summarize geographic characteristics, such as population density, pollution levels, and per capita income, among others.
+    content: `A <a href="https://www.tableau.com/data-insights/reference-library/visual-analytics/geospatial/area-maps">Choropleth map</a> is a type of <b>statistical thematic map</b> that utilizes color to represent data. The colors correspond to aggregates of data that summarize geographic characteristics, such as population density, pollution levels, and per capita income, and other spatial data.
     <br><br>
-    [insert image]`
+    <img src="/images/js_finalBivariateMap.png" alt="choropleth example" width="600px" />`
   },
   { 
     title: 'Implementation',
-    content: `Here is my Choropleth Map using Plotly and Pandas:
-    <br><br>
-    [insert visualization]`
-  },
-  { 
-    content: `**Figure 2** shows a choropleth map illustrating the total number of cars sold in each state in the US, featuring interactive annotations and a hover feature. Note that data is unavailable for certain states.
-    <br><br>
-    **Magnitude Channel**: Position, Area, Color saturation
-    **Identity Channel**: Spatial region, Color hue, Shape`
+    content: `Here is my Choropleth Map using Plotly and Pandas:`
   },
   { 
     title: 'Results',
-    content: `The intended user task was to 'Explore how vehicle sales differ by state.' The choropleth map emerged as an exemplary visualization for this purpose. Geometry and spatial data play a critical role in identifying states with the highest and lowest car sales. Additionally, it offers a simplistic overview for users to interact with. For instance, it includes an interactive hover feature displaying annotations such as state and sales, alongside a prominent color gradient indicating the states with the highest sales. This provides users ease of access and engagement with geographical data on cars. `
+    content: `The intended user task was to <b>'Explore how vehicle sales differ by state.'</b> The choropleth map emerged as an exemplary visualization for this purpose. Geometry and spatial data plays a critical role in identifying states with the highest and lowest car sales. Additionally, it offers a simplistic overview for users to interact with. For instance, it includes an interactive hover feature displaying annotations such as state and sales, alongside a prominent color gradient indicating the states with the highest sales. This provides users ease of access and engagement with geographical data on cars. `
   },
   { 
     title: 'Conclusion',
-    content: `In conclusion, my research has emphasized the value of **Marks & Channels** in data visualization. These two aspects illustrate both the expressiveness and effectiveness principles crucial for communicating visualizations to users, especially given the plethora of options available. The theme plays a pivotal role in ensuring that data is accurately represented and that the underlying ideas are effectively conveyed.
+    content: `In conclusion, my research has emphasized the value of <b>Marks & Channels</b> in data visualization. These two aspects illustrate both the expressiveness and effectiveness principles crucial for communicating visualizations to users, especially given the plethora of options available. The theme plays a pivotal role in ensuring that data is accurately represented and that the underlying ideas are effectively conveyed.
     <br><br>
-    Moreover, the audience was central to considering how Marks & Channels could effectively tell a story. Our targeted users have specific points of interest for research, decision-making, and marketing purposes. I believe these visualization techniques effectively conveyed the intended tasks and met the requirements necessary to fulfill these questions.`
+    Moreover, the audience was central to considering how Marks & Channels could effectively tell a story. Our <b>targeted users</b> have specific points of interest for research, decision-making, and marketing purposes. I believe these visualization techniques effectively conveyed the intended tasks and met the requirements necessary to fulfill these questions.`
   },
   { 
     title: 'References',
-    content: `[1] N. Psychonaut, “Streamgraph,” Wikipedia, https://en.wikipedia.org/wiki/Streamgraph#/media/File:LastGraph_example.svg (accessed May 2, 2024). `
+    content: `
+    [1] J. Korenblat, “Mindful tools for data exploration (or, how to make visual lasagna),” Medium, https://medium.com/@JoshuaKorenblat/mindful-tools-for-data-exploration-or-how-to-make-visual-lasagna-57a3929e31ce (accessed May 3, 2024). 
+    <br><br>
+    [2] T. Munzner, “Chapter 5 Marks and Channels,” in Visualization Analysis and Design, Boca Raton, FL: CRC Press, 2014, pp. 95–116 
+    <br><br>
+    [2] N. Psychonaut, “Streamgraph,” Wikipedia, https://en.wikipedia.org/wiki/Streamgraph#/media/File:LastGraph_example.svg (accessed May 2, 2024).
+    <br><br>
+    [3] J. Stevens, “Bivariate Choropleth Maps: A how-to guide,” Joshua Stevens - Visual Communicator, https://www.joshuastevens.net/cartography/make-a-bivariate-choropleth-map/ (accessed May 3, 2024).  
+    <br>
+    [4] N. nolimits4web, “Nolimits4web/swiper: Most modern mobile touch slider with hardware accelerated transitions,” GitHub, https://github.com/nolimits4web/swiper (accessed May 3, 2024). `
   },
 ];
 
@@ -151,6 +147,7 @@ function handleStepEnter({ element }) {
         <h2>{{ step.title }}</h2>
         <div v-html="step.content" class="content"></div> 
         <div v-if="index === 4" class="streamgraph">
+          <img src="../assets/Screenshot 2024-05-03 123139.png" alt="streamgraph" width="90%" />
           <Swiper
           :modules="[Navigation, Pagination, Autoplay]"
           :pagination="{ clickable: true }"
@@ -161,10 +158,22 @@ function handleStepEnter({ element }) {
               <img :src="require(`../assets/${slide}`)" alt="" width="100%"/>
             </SwiperSlide>
           </Swiper>
+          <p><b>Figure 1</b> presents a revised streamgraph depicting the yearly market trends (MMR) by car make and year, featuring additional interactivity such as a scrollable legend, year slider, and reset buttons.</p>
+          <br>
+          <p><b>Magnitude Channel</b>: Position, Area</p>
+          <p><b>Identity Channel</b>: Spatial position, Color Hue</p>
         </div> 
-        <!-- <div v-if="index === 1" class="choropleth-map">
+        <div v-if="index === 7" class="choropleth-map">
           <iframe src="/images/ChoroplethMap.html"></iframe>
-        </div>      -->
+          <div>
+            <p>
+              <b>Figure 2</b> shows a choropleth map illustrating the total number of cars sold in each state in the US, featuring interactive annotations and a hover feature. Note that data is unavailable for certain states.
+            </p>
+            <br>
+            <p><b>Magnitude Channel</b>: Position, Area, Color saturation</p>
+            <p><b>Identity Channel</b>: Spatial region, Color hue, Shape</p>
+          </div>
+        </div>     
       </v-col>
     </VueScrollama>
   </div>
@@ -182,21 +191,32 @@ function handleStepEnter({ element }) {
 }
 
 .content{
-  padding-bottom: 24px;
+  padding: 0 64px;
 }
 
 .step {
-  margin: 128px 0;
+  margin: 0;
+  padding: 24px;
   width: 100%;
-  min-height: 70vh;
+  height: max-content;
   transition: transform 0.3s ease-in-out, opacity 0.3s ease-in-out;
   transform: translateY(20px); 
   opacity: 0.5; /* Start with a lower opacity */
 }
 
+.step h2 {
+  font-size: 2rem;
+  font-weight: 700;
+}
+
 .step-active {
   transform: translateY(0); /* End at natural position */
   opacity: 1; /* Fully visible when active */
+}
+
+.step:nth-child(11) .content{
+  text-align: left;
+  margin-bottom: 80px;
 }
 
 .scrollama-container .step p {
@@ -208,20 +228,21 @@ function handleStepEnter({ element }) {
 .swiper {
   margin: auto;
   width: 100%;
-  height: 600px;
+  min-height: 100px;
+  max-height: max-content;
   overflow: visible;
 }
 
 .swiper-slide img {
   width: 90%;
-  height: 100%;
+  height: 600px;
   object-fit: contain;
 }
 
 .streamgraph {
-  margin: auto;
-  width: 100;
-  height: 600px;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
   justify-content: center;
   align-items: center;
 }
@@ -230,6 +251,7 @@ function handleStepEnter({ element }) {
   margin: auto;
   width: 100;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 }
